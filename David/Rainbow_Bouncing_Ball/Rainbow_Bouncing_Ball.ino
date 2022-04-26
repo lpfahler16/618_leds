ac5ade8983517a9933bd983378271b0f7a7464a7
dweger734 <92614913+dweger734@users.noreply.github.com>
2022-04-25 21:04:30 -0400

    Updated the bouncy balls

--- David/Rainbow_Bouncing_Ball/Rainbow_Bouncing_Ball.ino
+++ David/Rainbow_Bouncing_Ball/Rainbow_Bouncing_Ball.ino
@@ -8,6 +8,7 @@ int i = 0;
 int x = 0;
 uint32_t color[8] = {0xFF0000, 0xFFA500, 0xFFFF00, 0x00FF00, 0x40E0D0, 0x0000FF, 0x800080, 0x800000};
 bool goUp = true;
+bool changeColor = false;
   
 void setup() {
   
@@ -23,25 +24,35 @@ void loop() {
 }
 
 void UpwardSpiral() {
-  lamp.Clear();
+  lamp.SetLamp(0x202020);
   lamp.SetRings(i, i+3, color[x]);
-  lamp.Delay(50);
+  lamp.Delay(75);
 
   i = i+3;
-  x = x+1;
-  x = x % 8;
+  if(changeColor) {
+    x = x+1;
+    x = x % 8;
+    changeColor = false;
+  }
+  else
+    changeColor = true;
   if(i > lamp.LENGTH) 
     goUp = false;
 }
 
 void DownwardSpiral() {
-  lamp.Clear();
+  lamp.SetLamp(0x202020);
   lamp.SetRings(i-3, i, color[x]);
-  lamp.Delay(50);
+  lamp.Delay(75);
 
   i = i-3;
-  x = x+1;
-  x = x % 8;
+  if(changeColor) {
+    x = x+1;
+    x = x % 8;
+    changeColor = false;
+  }
+  else
+    changeColor = true;
   if(i < 2) 
     goUp = true;
 }
